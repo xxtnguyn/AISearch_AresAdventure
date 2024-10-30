626ee2163c084553fdc11e3b839662a868304ce4
MinhHung7 <hungm0434@gmail.com>
2024-10-30 20:13:28 +0700

    Xóa toàn bộ file cũ

--- readinput.py
+++
@@ -1,9 +0,0 @@
-def load_grid_from_file(filename):
-    """Load the game grid from a file"""
-    grid = []
-    with open(filename, 'r') as file:
-        file.readline()  # Skip dimensions line
-        for line in file:
-            line = line[:-1]
-            grid.append(list(line))  # Convert each line into a list of characters
-    return grid
